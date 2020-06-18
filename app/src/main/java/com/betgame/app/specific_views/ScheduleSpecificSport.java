e0184e115844f1ae91b14aac2ab0aaa47ad83dde
Finn Klapper <finn.klapper77@gmail.com>
2020-06-18 23:11:55 +0200

    Fixed error to now display Games for Sports properly. Needs implementation of League Query now!

--- app/src/main/java/com/betgame/app/specific_views/ScheduleSpecificSport.java
+++ app/src/main/java/com/betgame/app/specific_views/ScheduleSpecificSport.java
@@ -26,21 +26,25 @@ public class ScheduleSpecificSport extends Fragment implements ScheduleFragmentA
     private static final String GameArrayKey = "GameArray";
     private static final String StringKey = "SelectedSport";
     private ArrayList<Parcelable> mGameArray;
+    private String mSportType;
 
     public static ScheduleSpecificSport newInstance(ArrayList<Parcelable> games, String selectedSports) {
         ScheduleSpecificSport fragment = new ScheduleSpecificSport();
+        Bundle initiativeBundle = new Bundle();
         Bundle bundle1 = new Bundle();
         Bundle bundle2 = new Bundle();
         bundle1.putParcelableArrayList(GameArrayKey, games);
         bundle2.putString(StringKey, selectedSports);
-        fragment.setArguments(bundle1);
-        fragment.setArguments(bundle2);
+        initiativeBundle.putBundle("BundleGameArray", bundle1);
+        initiativeBundle.putBundle("BundleSportsName", bundle2);
+        fragment.setArguments(initiativeBundle);
         return fragment;
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View myView = inflater.inflate(R.layout.explicit_sports_display, container, false);
-        mGameArray = getArguments() != null ? getArguments().getParcelableArrayList(GameArrayKey) : null;
+        mGameArray = getArguments() != null ? getArguments().getBundle("BundleGameArray").getParcelableArrayList(GameArrayKey) : null;
+        mSportType = getArguments() != null ? getArguments().getBundle("BundleSportsName").getString(StringKey) : null;
         rv_explicit_sports_display = (RecyclerView) myView.findViewById(R.id.rv_explicit_sports_display);
         LinearLayoutManager schedule_fragment_layout_manager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         rv_explicit_sports_display.setLayoutManager(schedule_fragment_layout_manager);
@@ -64,6 +68,8 @@ public class ScheduleSpecificSport extends Fragment implements ScheduleFragmentA
     @Override
     public void onClick(String weatherForDay) {
         Intent intent = new Intent(getContext(), GamesForSport.class);
+        intent.putParcelableArrayListExtra("Extra", mGameArray);
+        intent.putExtra("SportType", mSportType);
         startActivity(intent);
     }
 }
